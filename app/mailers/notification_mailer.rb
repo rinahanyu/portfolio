18ad799565e688024993d933f0c190dcd8c459bc
EC2 Default User <ec2-user@ip-172-31-18-244.ec2.internal>
2020-12-12 14:30:17 +0000

    [Fix]mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   def medical_record_to_user(user, medical_record)
     @user = user
     @medical_record = medical_record
-    @url = "https://joy_health.com/users/sign_in"
+    @url = "https://joyhealth.work/users/sign_in"
     mail(to: @user.email, subject: '診療記録の新規登録があります')
   end
 end
