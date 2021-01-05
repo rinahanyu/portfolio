61d605189e1456ded3e7cda6915760a659fac396
EC2 Default User <ec2-user@ip-172-31-18-244.ec2.internal>
2021-01-05 06:59:10 +0000

    [Fix]daily_record model_test

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :address, :email, 
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :address, :email,
             presence: true
   validates :telphone_number, :postal_code, numericality: { only_integer: true }
   attachment :profile_image
--- spec/models/daily_record_spec.rb
+++ spec/models/daily_record_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe DailyRecord, type: :model do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
   context "データが正しく保存される" do
     before do
-      @daily_record = DailyRecord.new
-      @daily_record.user_id = 1
-      @daily_record.theme = "題名"
-      @daily_record.introduction = "内容"
-      @daily_record.genre = 0
-      @daily_record.save
+      @daily_record = FactoryBot.create(:daily_record, user: @user)
     end
 
     it "全て入力してあるので保存される" do
@@ -18,17 +17,14 @@ RSpec.describe DailyRecord, type: :model do
 
   context "データが正しく保存されない" do
     before do
-      @daily_record = DailyRecord.new
-      @daily_record.user_id = 1
-      @daily_record.genre = 0
-      @daily_record.save
+      @daily_record_a = FactoryBot.build(:daily_record, theme: "", introduction: "", user: @user)
+      @daily_record_a.valid?
     end
 
     it "theme, introductionが入力されていないので保存されない" do
-      expect(@daily_record).to be_invalid
-      expect(@daily_record.errors[:theme]).to include("を入力してください")
-      expect(@daily_record.errors[:introduction]).to include("を入力してください")
+      expect(@daily_record_a).to be_invalid
+      expect(@daily_record_a.errors[:theme]).to include("を入力してください")
+      expect(@daily_record_a.errors[:introduction]).to include("を入力してください")
     end
   end
-  # belongs_to :userを外して成功
 end
